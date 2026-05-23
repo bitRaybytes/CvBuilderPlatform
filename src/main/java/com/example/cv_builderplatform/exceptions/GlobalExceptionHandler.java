4263273a1669d4b06ea2d87879791845f1394a7f
bitRaybytes <173811320+bitRaybytes@users.noreply.github.com>
2026-05-23 23:22:12 +0200

    deleted unused imports

--- src/main/java/com/example/cv_builderplatform/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/cv_builderplatform/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,10 @@
 package com.example.cv_builderplatform.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.itextpdf.text.pdf.PdfStructTreeController.returnType;
 
 
 /**
