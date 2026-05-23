3159b1ff7db0a09b33558a1d5c044923eba2d0c5
bitRaybytes <173811320+bitRaybytes@users.noreply.github.com>
2026-05-23 23:31:23 +0200

    Note added to class for further actions if needed

--- src/main/java/com/example/cv_builderplatform/handler/FileUploadHandler.java
+++ src/main/java/com/example/cv_builderplatform/handler/FileUploadHandler.java
@@ -4,6 +4,18 @@ import java.io.IOException;
 
 import org.springframework.web.multipart.MultipartFile;
 
+/** NOTE
+ * 
+ * Files are only stored as string from filepath. 
+ * PdfExport has to read the local FS to access image.
+ * 
+ * I can set the return type String to byte[] @upload to store the actual image.
+ * 
+ * Possible options are: 
+ * Store as DB BLOB with `bytea` out of byte[]-Array – no FS needed, 
+ * or cloud storages like S3 / Azure Blob
+ */
+
 public interface FileUploadHandler {
     String upload(MultipartFile file, String username) throws IOException;
     void validate(MultipartFile file, long maxBytes);
