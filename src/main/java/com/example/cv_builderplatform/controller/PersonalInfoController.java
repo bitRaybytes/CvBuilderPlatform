2b9c3531a5989eb8237ea7f43f9037619efbecca
bitRaybytes <173811320+bitRaybytes@users.noreply.github.com>
2026-05-23 23:22:45 +0200

    organized imports

--- src/main/java/com/example/cv_builderplatform/controller/PersonalInfoController.java
+++ src/main/java/com/example/cv_builderplatform/controller/PersonalInfoController.java
@@ -1,6 +1,5 @@
 package com.example.cv_builderplatform.controller;
 
-import com.example.cv_builderplatform.dto.cv.CvResponseDTO;
 import com.example.cv_builderplatform.dto.cv.PersonalInfoDTO;
 import com.example.cv_builderplatform.services.PersonalInfoService;
 import lombok.RequiredArgsConstructor;
@@ -8,22 +7,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/cv/personals")
 public class PersonalInfoController {
 
-    private final PersonalInfoService personalInfos;
+    private final PersonalInfoService service;
 
 
     // /saveCv
     @PostMapping
-    public ResponseEntity<CvResponseDTO> saveCv(
+    public ResponseEntity<PersonalInfoDTO> saveCv(
             @AuthenticationPrincipal UserDetails userDetails,
             @RequestBody PersonalInfoDTO personalDTO) {
-        CvResponseDTO personals =
-                personalInfos.savePersonalInfoByUsername(userDetails.getUsername(),personalDTO);
+        PersonalInfoDTO personals =
+                service.savePersonalInfoByUsername(userDetails.getUsername(),personalDTO);
         return ResponseEntity.ok(personals);
     }
 
@@ -32,8 +32,16 @@ public class PersonalInfoController {
     @DeleteMapping
     public ResponseEntity<Void> deleteCv(
             @AuthenticationPrincipal UserDetails userDetails) {
-        personalInfos.deletePersonalInfoByUsername(userDetails.getUsername());
+        service.deletePersonalInfoByUsername(userDetails.getUsername());
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/upload-profile-picture")
+    public ResponseEntity<PersonalInfoDTO> uploadProfilePicture(
+        @AuthenticationPrincipal UserDetails userDetails,
+        @RequestParam("file") MultipartFile file){
+            PersonalInfoDTO saved = service.uploadProfilePicture(userDetails.getUsername(), file);
+            return ResponseEntity.ok(saved);
     }
 
 }
